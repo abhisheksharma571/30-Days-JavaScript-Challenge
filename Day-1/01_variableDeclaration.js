050aa65ce2e4dc058c4da458c66e8f12b5301c15
Abhishek Sharma <sharmabhishek802158@gmail.com>
2024-07-14 03:01:17 +0530

    Day 1 task completed

---
+++ Day-1/01_variableDeclaration.js
@@ -0,0 +1,10 @@
+// Task 1: Declare a variable using var, assign it a number, and log the value to the console.
+var x = 10;
+console.log(x);
+
+// Task 2: Declare a variable using let, assign it a string, and log the value to the console.
+let y = "Hello";
+console.log(y)
+
+
+// var and let keyword is used to declare a variable
---
+++ Day-1/02_constantdeclaration.js
@@ -0,0 +1,5 @@
+//Task 3: Declare a variable using const, assign it a boolean value, and log the value to the console.
+const isTrue = true;
+console.log(isTrue);
+
+//const keyword is used to declare a constant which is a variable whose value cannot be changed
