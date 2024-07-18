0195ddfb9e5add0f611d3376b0761b2b56271e00
Abhishek Sharma <sharmabhishek802158@gmail.com>
2024-07-18 08:03:49 +0530

    Day-06 task completed

---
+++ Day-06/arrayCreationAndAccess.js
@@ -0,0 +1,7 @@
+//TAsk 1: Create and array of numbers from 1 to 5 and log the array to the console.
+let arr = [1, 2, 3, 4, 5];
+console.log(arr);
+
+//Task 2: Access the first and last elements of the array and log them to the console.
+console.log(arr[0]);
+console.log(arr[arr.length - 1]);
